0b8557445d7ce9f60d8d209ad9449d738776c718
anshul202 <sainian45@gmail.com>
2024-10-18 15:51:05 +0530

    initial commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { SignIn } from "@clerk/clerk-react"
+const Signin = () => {
+  return (
+    <div><SignIn/></div>
+  )
+}
+
+export default Signin
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs'
+
+
+const Sign_up = () => {
+    return (
+      <div><SignUp/></div>
+    )
+  }
+  
+  export default Sign_up
