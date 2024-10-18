14fea4071841d404fb26062232a2c48dd6c5b99c
anshul202 <sainian45@gmail.com>
2024-10-18 17:05:36 +0530

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,20 @@ import type { Metadata } from 'next'
 export const metadata: Metadata ={
   title: 'Ask Me Anything',
   description: 'Ask Me Anything is a platform for asking questions and getting answers.',
+  icons: 'public\assets\images\site-logo.svg'
 }
+import './globals.css'
+const inter=Inter({
+  subsets:['latin'],
+  weight:['100','200','300','400','500','600','700','800','900'],
+  variable: '--font-inter'
+}
+);
+const spaceGrotesk=Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300','400','500','600','700'],
+  variable: '--font-spaceGrotesk'
+})
 
 
 export default function RootLayout({
@@ -20,9 +33,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance={{
+      elements:{
+        formButtonPrimary:'primary-gradient',
+        footerActionLink:'primary-text-gradient hover :primary-text-500',
+      }
+    }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`} >
           <SignedOut>
             <SignInButton />
           </SignedOut>
